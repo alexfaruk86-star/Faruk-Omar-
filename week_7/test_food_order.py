a3d00cec598c4ab853193e58d5b0a73c2ac6bfbe
Faruk Omar <alexfaruk86@gmail.com>
2026-07-08 16:04:55 +0800

    Add unit tests for calculate_total function

--- week_7/test_food_order.py
+++ week_7/test_food_order.py
@@ -1,1 +1,26 @@
+from food_order import calculate_total
 
+def test_order1():
+    assert calculate_total(10, 2) == 20
+
+def test_order2():
+    # test if total food order is equal to 30
+    assert calculate_total(10, 3) == 30
+
+def test_order3():
+    # test if total food order is equal to 100
+    assert calculate_total(50, 2) == 100
+
+def test_order4():
+    # test if total food order is equal to 10
+    assert calculate_total(5, 2) == 10
+
+def test_order5():
+    # test if total food order is equal to "invalid price"
+    assert calculate_total(-5, 2) == "invalid price"
+    assert calculate_total(0, 2) == "invalid price"
+
+def test_order6():
+    # test if total food order is equal to "invalid quantity"
+    assert calculate_total(10, -1) == "invalid quantity"
+    assert calculate_total(10, 0) == "invalid quantity"
